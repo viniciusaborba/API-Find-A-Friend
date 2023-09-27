758967fbf73f076936708c526c9ba865ffe7943f
Vinícius Alves Borba <viniboal28@gmail.com>
2023-09-27 09:57:31 -0300

    chore: added admin verification to register pet

--- src/http/controllers/pets/routes.ts
+++ src/http/controllers/pets/routes.ts
@@ -3,9 +3,10 @@ import { verifyJWT } from "../../middlewares/verify-jwt";
 import { registerPet } from "./register";
 import { petDetails } from "./details";
 import { fetchByCity } from "./fetch";
+import { verifyUserRole } from "../../middlewares/verify-user-role";
 
 export async function petsRoutes(app: FastifyInstance) {
-   app.post('/pets/:orgId/register', { onRequest: [verifyJWT] }, registerPet)
+   app.post('/pets/:orgId/register', { onRequest: [verifyJWT, verifyUserRole('ADMIN')] }, registerPet)
    app.get('/pets/list', fetchByCity)
 
    app.get('/pets/:petId/details', petDetails)
